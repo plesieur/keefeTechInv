5678141d0236cf313053b380046e3eb6acc62f47
Reilly <Reilly@DESKTOP-OMBC4I9>
2024-01-11 12:16:24 -0500

    classes now auto add the total of missing items

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const classes = ["senior", "freshman"]
 
 function dropDownClick(year) {
   if(document.getElementById(year + "Drop").innerHTML.charCodeAt(0) == "8595"){
@@ -9,5 +10,24 @@ function dropDownClick(year) {
     document.getElementById(year).style.display = "none";
   }
   console.log(document.getElementById(year + "Drop").innerHTML.codePointAt(0));
+  updateNumber();
 }
 
+function updateNumber(){
+  classes.forEach(numMath);
+}
+
+function numMath(year){
+  var total = 0;
+  for (let i = 1; i < document.getElementById(year).rows.length; i++) {
+    if(document.getElementById(year).rows[i].cells[2].innerHTML != "none"){
+      total += parseInt(document.getElementById(year).rows[i].cells[2].innerHTML, 10);
+    }
+ 
+  }
+
+  if(total <= 0){
+    total = "none";
+  }
+  document.getElementById(year + "Total").innerHTML = total;
+}
