ee9cba690419e86063d3c703e76f2f7731b3acd5
Jack <Jack@DESKTOP-11DSLI1>
2024-02-07 12:31:20 -0500

    tried moving

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function prpfile(name){
 function popfile(email){
   document.getElementById("Email").innerHTML = email;
 }
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+            
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
